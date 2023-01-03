9a999faeacce0a2494ec13732716716173288160
Oleksii Honchar <alekseyhonchar@gmail.com>
2023-01-03 15:41:59 +0000

    refactor: refactored  editOrSend function

--- src/helpers/edit-or-send.helper.ts
+++ src/helpers/edit-or-send.helper.ts
@@ -2,25 +2,31 @@ import { InlineKeyboardButton } from 'telegraf/typings/core/types/typegram';
 import { BotContext } from 'bot';
 import { editMessageByID } from 'helpers/message-editer.helper';
 import { sendMessage } from 'helpers/message-sender.helper';
-import { message } from 'telegraf/filters';
 
 const editOrSend =
   async (ctx: BotContext, data: string, keyboard: InlineKeyboardButton[][]) => {
-    try {
-      await editMessageByID(
+    const messageID = ctx.session.messageID,
+          chatID = ctx.session.chatID;
+
+    const messageExists = chatID && messageID;
+
+    if (messageExists) {
+      return editMessageByID(
         ctx,
         data,
         keyboard,
       );
-    } catch {
-      const message = await sendMessage(
-        ctx,
-        data,
-        keyboard,
-      );
-      ctx.session.messageID = message.message_id;
-      ctx.session.chatID = message.chat.id;
     }
+
+    const sentMessage = await sendMessage(
+      ctx,
+      data,
+      keyboard,
+    );
+    ctx.session.messageID = sentMessage.message_id;
+    ctx.session.chatID = sentMessage.chat.id;
+
+    return sentMessage;
   };
 
 export { editOrSend };
