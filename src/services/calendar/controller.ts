2c9237628dac275c2bd8e70a0de208112dbf763d
Oleksii Honchar <alekseyhonchar@gmail.com>
2023-01-11 15:56:21 +0000

    feat: basic work with Google Calendar API

--- src/services/calendar/controller.ts
+++ src/services/calendar/controller.ts
@@ -1,20 +1,31 @@
+import { Types } from 'mongoose';
 import { Calendar } from 'services/calendar/service';
 import { CalendarModel } from 'services/calendar/model';
 import { IEvent } from 'services/event';
+import { ICalendar } from './interface';
 
 const CalendarController = {
 
-  async save(calendar: Calendar) {
-    const calendarModel =
-      new CalendarModel(calendar.convertToObject());
-    await calendarModel.save();
+  async save(calendar: Calendar): Promise<ICalendar | undefined | null> {
+    const calendarExists = await this.exists(calendar.id);
+    const calendarObject = calendar.convertToObject();
+    if (calendarExists) {
+      return CalendarModel.findOneAndUpdate(
+        { _id: calendar.id }, calendarObject);
+    }
+    const calendarModel = new CalendarModel(calendarObject);
+    return calendarModel.save();
   },
 
-  async getByID(id: string | undefined): Promise<Calendar | undefined> {
+  async getByID(id: Types.ObjectId | undefined): Promise<Calendar | undefined> {
     if (!id) return undefined;
     const calendar = await CalendarModel.findOne({ _id: id });
     if (!calendar) return undefined;
     return Calendar.parse(calendar);
+  },
+
+  async exists(id: Types.ObjectId | undefined): Promise<boolean> {
+    return (await CalendarModel.find({ _id: id })).length > 0;
   },
 
   async insertEvent(calenadar: Calendar, event: IEvent) {
--- src/services/calendar/interface.ts
+++ src/services/calendar/interface.ts
@@ -1,8 +1,8 @@
 import { Types } from 'mongoose';
 
 interface ICalendar {
-  _id: Types.ObjectId,
-  calendarID: string,
+  _id?: Types.ObjectId,
+  calendarID?: string,
 }
 
 export { ICalendar };
